0d1fec135b7ca5ea9e165dfbf0dd647eb767f39b
JoyceGray-11 <grayj7087@student.faytechcc.edu>
2026-04-06 18:29:17 -0400

    Add files via upload

---
+++ P1LAB1_GrayJoyce.py
@@ -0,0 +1,9 @@
+# Joyce Gray
+# April 5, 2026
+# P1LAB1
+# print ang output
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!", "Welcome to CTI-110!")
